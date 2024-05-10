b43ed9775a73aa77103018a94979491a34dfc8df
xasannosirov <xasannosirov094@gmail.com>
2024-05-10 18:27:53 +0500

    edited api host

--- api-gateway/internal/pkg/config/config.go
+++ api-gateway/internal/pkg/config/config.go
@@ -86,7 +86,7 @@ func NewConfig() (*Config, error) {
 	config.Context.Timeout = getEnv("CONTEXT_TIMEOUT", "30s")
 
 	// server configuration
-	config.Server.Host = getEnv("SERVER_HOST", "localhost")
+	config.Server.Host = getEnv("SERVER_HOST", "api")
 	config.Server.Port = getEnv("SERVER_PORT", ":5555")
 	config.Server.ReadTimeout = getEnv("SERVER_READ_TIMEOUT", "10s")
 	config.Server.WriteTimeout = getEnv("SERVER_WRITE_TIMEOUT", "10s")
